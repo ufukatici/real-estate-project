ebbda41c3666d5f49e9d648668b51f92f4a26708
Ufuk Atıcı <ufuk.development@gmail.com>
2025-12-23 14:00:07 +0300

    Delete real-estate directory

--- real-estate/backend/properties/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Property, Inquiry
-
-admin.site.register(Property)
-admin.site.register(Inquiry)
-
--- real-estate/backend/real_estate/__init__.py
+++
@@ -1,7 +0,0 @@
-from django import setup
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate.settings')
-django.setup()
-
